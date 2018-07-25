cb558b1f20625deca87560f8b1cdec6a817a3a23
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-25 15:28:55 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,13 +11,14 @@ def change_rainbow_colors
   RAINBOW_COLORS[0] = "red"
   RAINBOW_COLORS[1] = "light_red"
   RAINBOW_COLORS[2] = "light_yellow"
+  RAINBOW_COLORS
 end
 
 def add_colors
   #Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
   RAINBOW_COLORS.push ("green")
   RAINBOW_COLORS.push ("blue")
+  RAINBOW_COLORS
 end
 change_rainbow_colors
 add_colors
-puts RAINBOW_COLORS
